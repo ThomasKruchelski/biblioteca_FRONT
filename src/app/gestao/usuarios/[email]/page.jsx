8e90b551f0117625a519e7eae7ce62346259c238
Thomas Kruchelski <thomas.lima.kru@gmail.com>
2024-10-27 01:57:15 -0300

    Edit do crud de usuário

--- src/app/gestao/usuarios/[email]/page.jsx
+++ src/app/gestao/usuarios/[email]/page.jsx
@@ -5,6 +5,9 @@ import { useEffect, useState } from 'react';
 import { isTokenValid, clearExpiredToken } from '@/utils/verificaToken'
 import Header from '@/components/Header';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function perfil({ params }) {
 
     const [usuario, setUsuario] = useState({})
@@ -70,11 +73,57 @@ export default function perfil({ params }) {
 
         setUsuario((prevState) => ({
             ...prevState,
-            tipoUsuario:{
+            tipoUsuario: {
+                ...prevState.tipoUsuario,
                 [name]: value
             }
         }));
     };
+
+
+    const fetchAlteraUsuario = async () => {
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/usuarios/email/${queryEmail}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'aplication/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify(usuario)
+            });
+
+            if (!response.ok) {
+                toast.error(`Erro ao alterar usuário`, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+
+                });
+                throw new Error('Erro ao alterar usuário');
+            }
+
+            const data = await response.json();
+            toast.success(`Usuário alterado com sucesso`, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+
+            });
+        } catch (error) {
+            console.error('Erro:', error);
+        }
+    };
+
 
     return (
         <main className=" bg-gray-100 text-gray-900 flex flex-col min-h-[100vh]">
@@ -119,8 +168,9 @@ export default function perfil({ params }) {
                             <a href='./'>
                                 <div className='px-4 py-2 rounded bg-[#cc2222] text-white'>Cancelar</div>
                             </a>
-                            <div className='px-4 py-2 rounded bg-[#669966] text-white'>Salvar</div>
+                            <div onClick={() => fetchAlteraUsuario()} className='px-4 py-2 rounded bg-[#669966] text-white'>Salvar</div>
                         </div>
+                        <ToastContainer />
                     </div>
                 }
             </section>
