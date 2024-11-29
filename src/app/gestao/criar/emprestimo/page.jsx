344a004288d1661e4a14c8eee1c3a334b201403e
Thomas Kruchelski <thomas.lima.kru@gmail.com>
2024-11-29 19:24:33 -0300

    working create emprestimo

--- src/app/gestao/criar/emprestimo/page.jsx
+++ src/app/gestao/criar/emprestimo/page.jsx
@@ -4,15 +4,16 @@ import { useParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { isTokenValid, clearExpiredToken } from '@/utils/verificaToken'
 import Header from '@/components/Header';
+import Select from 'react-select'
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export default function editora({ params }) {
+export default function emprestimo({ params }) {
 
     const router = useRouter();
 
-    const [editora, seteditora] = useState({
+    const [emprestimo, setemprestimo] = useState({
         livro: {
             titulo: ""
         },
@@ -23,94 +24,134 @@ export default function editora({ params }) {
             }
         }
     })
+
+    const [usuarios, setUsuarios] = useState([])
+
+    const [options, setOptions] = useState([])
+
+    const [selectedOption, setSelectedOption] = useState(null)
+
     const [loaded, setLoaded] = useState(false)
     const [token, setToken] = useState({})
 
-    const queryParams = useParams();
-    const queryEmail = decodeURIComponent(queryParams.email)
-    console.log(queryEmail)
+    // const queryParams = useParams();
+    // const queryEmail = decodeURIComponent(queryParams.email)
+    // console.log(queryEmail)
 
     useEffect(() => {
-        console.log(JSON.stringify(editora))
-    }, [editora])
+        if(selectedOption != null){
+            setemprestimo((prevState) => ({
+                ...prevState,
+                usuario: selectedOption.value
+            }));
+        }
+       
+    }, [selectedOption])
+
+    useEffect(() => {
+        console.log(JSON.stringify(emprestimo))
+    }, [emprestimo])
+
+    useEffect(() => {
+        console.log(JSON.stringify(options))
+    }, [options])
 
     useEffect(() => {
         if (isTokenValid()) {
             // console.log(isTokenValid(false))
             // console.log('isTokenValid()')
             setToken(isTokenValid(false))
-            setLoaded(true)
+            // setLoaded(true)
         } else {
             clearExpiredToken()
             router.push('/login');
         }
     }, [])
 
-    // useEffect(() => {
-    //     const fetcheditora = async () => {
-    //         try {
-    //             const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/editoras/email/${queryEmail}`, {
-    //                 method: 'GET',
-    //                 headers: {
-    //                     'Content-Type': 'application/json',
-    //                     'Authorization': `Bearer ${token}`
-    //                 },
-    //             });
+    useEffect(() => {
+        const fetchUsuario = async () => {
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/usuarios`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                });
 
-    //             if (!response.ok) {
-    //                 throw new Error('Erro ao buscar editoras');
-    //             }
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar usuários');
+                }
 
-    //             const data = await response.json();
-    //             seteditora(data);
-    //             setLoaded(true)
-    //         } catch (error) {
-    //             console.error('Erro:', error);
-    //         }
-    //     };
-    //     fetcheditora()
-    // }, [token])
+                const data = await response.json();
+                setUsuarios(data);
+                setLoaded(true)
+            } catch (error) {
+                console.error('Erro:', error);
+            }
+        };
+        fetchUsuario()
+    }, [token])
 
     useEffect(() => {
-        console.log(editora)
-        console.log('editora')
-    }, [editora])
+        try {
+            const opcoes = usuarios.map((usuario) => (
+                {
+                    label: usuario.nome,
+                    value: {
+                        id: usuario.id,
+                        tipo_usuario: {
+                            descricao: usuario.tipoUsuario.descricao
+                        }
+                    }
+                }
+            ))
+            setOptions(opcoes)
+        } catch (error) {
+            console.error('erro nos usuários ' + error)
+        }
+    }, [usuarios])
+
+    useEffect(() => {
+        console.log(emprestimo)
+        console.log('emprestimo')
+    }, [emprestimo])
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
 
-        seteditora((prevState) => ({
+        setemprestimo((prevState) => ({
             ...prevState,
-            [name]: value
+            livro: { titulo: value }
         }));
     };
 
-    const handleInputChangeTipoeditora = (e) => {
+    const handleInputChangeTipoemprestimo = (e) => {
         const { name, value } = e.target;
 
-        seteditora((prevState) => ({
+        setemprestimo((prevState) => ({
             ...prevState,
-            tipoeditora: {
-                ...prevState.tipoeditora,
+            tipoemprestimo: {
+                ...prevState.tipoemprestimo,
                 [name]: value
             }
         }));
     };
 
 
-    const fetchAlteraeditora = async () => {
+    const fetchAlteraemprestimo = async () => {
         try {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/editoras`, {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/emprestimos`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${token}`
                 },
-                body: JSON.stringify(editora)
+                body: JSON.stringify(emprestimo)
             });
 
             if (!response.ok) {
-                toast.error(`Erro ao criar editora`, {
+                toast.error(`Erro ao criar emprestimo`, {
                     position: "top-right",
                     autoClose: 5000,
                     hideProgressBar: false,
@@ -121,13 +162,13 @@ export default function editora({ params }) {
                     theme: "colored",
 
                 });
-                throw new Error('Erro ao criar editora');
+                throw new Error('Erro ao criar emprestimo');
             }
 
             const data = await response.json();
             console.log(data)
             console.log('data')
-            toast.success(`editora alterada com sucesso`, {
+            toast.success(`emprestimo alterada com sucesso`, {
                 position: "top-right",
                 autoClose: 5000,
                 hideProgressBar: false,
@@ -138,7 +179,7 @@ export default function editora({ params }) {
                 theme: "colored",
 
             });
-            router.push('/gestao/editoras');
+            router.push('/gestao/emprestimos');
         } catch (error) {
             console.error('Erro:', error);
         }
@@ -151,28 +192,27 @@ export default function editora({ params }) {
             <section className="container bg-purple-50 mx-auto mt-8 px-4 flex flex-col flex-1 h-[100vh]">
                 {loaded &&
                     <div className='flex flex-col'>
-                        <h2 className="text-3xl font-semibold mb-6 mt-6 text-purple-700 text-center">Criando {editora.nome}</h2>
+                        <h2 className="text-3xl font-semibold mb-6 mt-6 text-purple-700 text-center">Criando {emprestimo.nome}</h2>
                         <div className='flex flex-col mb-4'>
 
                             <label className='flex flex-col'>
                                 <p className='ml-2'>
-                                    Nome da Editora
+                                    Nome da emprestimo
                                 </p>
-                                <input
-                                    className='mb-2 px-2 py-1 border shadow-inner rounded-full'
-                                    value={editora.nome}
-                                    name='nome'
-                                    onChange={handleInputChange}
-                                ></input>
+                                <Select
+                                    options={options}
+                                    onChange={setSelectedOption}
+                                    value={selectedOption}
+                                />
                             </label>
                             <label className='flex flex-col'>
                                 <p className='ml-2'>
-                                    Endereço da Editora
+                                    Endereço da emprestimo
                                 </p>
                                 <input
                                     className='mb-2 px-2 py-1 border shadow-inner rounded-full'
-                                    value={editora.endereco}
-                                    name='endereco'
+                                    value={emprestimo.livro.titulo}
+                                    name='livro'
                                     onChange={handleInputChange}
                                 ></input>
                             </label>
@@ -181,7 +221,7 @@ export default function editora({ params }) {
                             <a href='./'>
                                 <div className='px-4 py-2 rounded bg-[#cc2222] text-white cursor-pointer'>Cancelar</div>
                             </a>
-                            <div onClick={() => fetchAlteraeditora()} className='px-4 py-2 rounded bg-[#669966] text-white cursor-pointer'>Salvar</div>
+                            <div onClick={() => fetchAlteraemprestimo()} className='px-4 py-2 rounded bg-[#669966] text-white cursor-pointer'>Salvar</div>
                         </div>
                         <ToastContainer />
                     </div>
